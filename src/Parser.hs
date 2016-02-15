bca6bc3667d3ed93d89a6ca57f386474757a9a8b
Jonas Betzendahl <jbetzend@techfak.uni-bielefeld.de>
2016-02-15 23:07:33 +0100

    First parser

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,1 +1,22 @@
 module Parser where
+
+import Types
+
+import Control.Applicative ((<|>))
+
+import Data.Attoparsec.Text
+import Data.Time
+
+parseUser :: Parser User
+parseUser = do name <- many1 letter
+               char ';'
+               balance <- decimal
+               char ';'
+               mail <- many' letter
+               let wrap = if mail == "" then Nothing else Just mail
+               char ';'
+               time <- parseUTC
+               return $ User name wrap balance time
+
+parseUTC :: Parser UTCTime
+parseUTC = undefined
--- src/Types.hs
+++ src/Types.hs
@@ -4,14 +4,22 @@ import Control.Monad.Trans.RWS.Lazy
 
 import Data.Time
 
-data User = User { name         :: String
-                 , mailaddress  :: Maybe String
-                 , balance      :: Integer
-                 , last_changed :: UTCTime  
-                 }
+data Env = Env { host         :: String       -- host for emails
+               , from_address :: String       -- sender address for angry emails
+               , cc_address   :: Maybe String -- CC address for angry emails
+               , thresh_low   :: Integer      -- Threshold (negative) for marking in the list
+               , thresh_lower :: Integer      -- Threshold (negative) for angry e-mails
+               , thresh_bonus :: Integer      -- Threshold (positive) for bonus treatment
+               } 
+
+data User  = User  { name         :: String
+                   , mailaddress  :: Maybe String
+                   , balance      :: Integer
+                   , last_changed :: UTCTime  
+                   }
 
 data State = State { list  :: [User]
                    , index :: Int
                    }
 
-type LambdaList = RWST () () State IO
+type LambdaList = RWST Env () State IO
